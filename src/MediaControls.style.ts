c479d7911fcbcc3675ff2b974caadca046597d80
soqq <35584085+enigmablue@users.noreply.github.com>
2020-08-24 19:50:43 +0800

    Update MediaControls.style.ts

--- src/MediaControls.style.ts
+++ src/MediaControls.style.ts
@@ -24,6 +24,7 @@ export default StyleSheet.create({
     alignSelf: "stretch",
     flex: 1,
     justifyContent: "center",
+    marginBottom: -150
   },
   fullScreenContainer: {
     alignItems: "center",
